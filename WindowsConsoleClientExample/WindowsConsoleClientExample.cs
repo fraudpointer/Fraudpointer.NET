c9fabb8b02f9fbdc9bdecc7e76b1ce3718d85946
Panayotis Matsinopoulos <p.matsinopoulos@fraudpointer.com>
2011-08-05 15:48:47 +0300

    Fixes #6400 Windows console application is now demonstrates the use of GetFraudAssessment

--- WindowsConsoleClientExample/WindowsConsoleClientExample.cs
+++ WindowsConsoleClientExample/WindowsConsoleClientExample.cs
@@ -12,7 +12,25 @@ namespace Fraudpointer.API
             Console.Error.WriteLine("   1st has to be the base url for the Fraudpointer API. Try: https://production.fraudpointer.com/api");
             Console.Error.WriteLine("   2nd has to be your API Key. You will find it in your Fraudpointer Application Account details.");
             Console.Error.WriteLine("   You can optionally pass the web request timeout as 3rd parameter (given in milliseconds)");
-        }
+        } // WrongSyntax ()
+        //------------------
+
+        static void PrintFraudAssessmentDetails (FraudAssessment fraudAssessment)
+        {
+            Console.WriteLine("...Fraud Assessment Id: " + fraudAssessment.Id);
+            Console.WriteLine("...Fraud Assessment Score: " + fraudAssessment.Score);
+            Console.WriteLine("...Fraud Assessment Result: " + fraudAssessment.Result);
+            Console.WriteLine("...Fraud Assessment Deciding Factor: " + fraudAssessment.DecidingFactor);
+            Console.WriteLine("...Fraud Assessment Updated At: " + fraudAssessment.UpdatedAt);
+            Console.WriteLine("......Profile Id: " + fraudAssessment.Profile.Id);
+            Console.WriteLine("......Profile Name: " + fraudAssessment.Profile.Name);
+            Console.WriteLine("......Profile Updated At: " + fraudAssessment.Profile.UpdatedAt);
+            Console.WriteLine("......Case Id: " + fraudAssessment.Case.Id);
+            Console.WriteLine("......Case Resolution: " + fraudAssessment.Case.Resolution);
+            Console.WriteLine("......Case Status: " + fraudAssessment.Case.Status);
+            Console.WriteLine("......Case Update At: " + fraudAssessment.Case.UpdatedAt);
+        } // PrintFraudAssessmentDetails ()
+        //----------------------------------
 
         static void Main(string[] args)
         {
@@ -47,9 +65,13 @@ namespace Fraudpointer.API
             try
             {
                 var client = ClientFactory.Construct(baseUrl, apiKey, webRequestTimeout);
-                AssessmentSession assessmentSession = client.CreateAssessmentSession();
-                Console.WriteLine("Assessement Session ID: " + assessmentSession.Id);
 
+                Console.WriteLine("About to Create an Assessment Session...");
+                AssessmentSession assessmentSession = client.CreateAssessmentSession();
+                Console.WriteLine("...done. Assessement Session ID: " + assessmentSession.Id);
+
+                //--------- Generic Event --------------------------------
+                Console.WriteLine("About to Create a Generic Event...");
                 Event l_event = new Event(Event.GenericEvent);
 
                 l_event.AddData("BookingCode", "1234");
@@ -59,21 +81,41 @@ namespace Fraudpointer.API
 
                 DateTime reservationDate = new DateTime(2001, 3, 21, 8, 0, 0, DateTimeKind.Local);
                 l_event.AddData("ReservationDate", reservationDate);
+                Console.WriteLine("...done");
 
-                client.AppendEventToAssessmentSession(assessmentSession, l_event);
+                Console.WriteLine("About to Send Event over to Fraudpointer Server...");
+                Event l_eventReturned = client.AppendEventToAssessmentSession(assessmentSession, l_event);
+                Console.WriteLine("...done. Event");
+                //--------------------------------------------------------
 
+                //-------- Failed Payment Event --------------------------
+                Console.WriteLine("About to Create a Failed Payment Event...");
                 l_event = new Event(Event.FailedPayment);
 
                 DateTime attemptDate = new DateTime(2011, 3, 22, 10, 0, 0, DateTimeKind.Local);
                 l_event.AddData("E_TRAVEL_SA_PURCHASE_DATE", attemptDate);
 
                 l_event.AddData("BILLING_ADDRESS_STREET_NAME", "Othonos");
+                Console.WriteLine("...done");
 
-                client.AppendEventToAssessmentSession(assessmentSession, l_event);
+                Console.WriteLine("About to Send Event over to Fraudpointer Server...");
+                l_eventReturned = client.AppendEventToAssessmentSession(assessmentSession, l_event);
+                Console.WriteLine("...done. Event");
+                //---------------------------------------------------------
 
+                //--------- Ask for Fraud Assessment ----------------------
+                Console.WriteLine("About to ask for Final Fraud Assessment");
                 FraudAssessment fa = client.CreateFraudAssessment(assessmentSession, false);
-                Console.WriteLine("FraudAssessment: ID:" + fa.Id + ", Score: " + fa.Score + ", fa: " + fa);
-                Console.ReadLine();
+                Console.WriteLine("...done");
+                PrintFraudAssessmentDetails(fa);
+                //---------------------------------------------------------
+
+                //---- For demo purposed I will now ask for the previously created Fraud Assessment ----
+                Console.WriteLine("About to ask for the previously created Fraud Assessment");
+                FraudAssessment fa_got = client.GetFraudAssessment(assessmentSession, fa.Id);
+                Console.WriteLine("...done");
+                PrintFraudAssessmentDetails(fa_got);
+                //--------------------------------------------------------------------------------------
                 
             } // try
             catch (Fraudpointer.API.ClientException ex)
@@ -86,5 +128,7 @@ namespace Fraudpointer.API
             } // catch  
 
         } // Main 
+        //---------
+
     } // class
 } // namespace
