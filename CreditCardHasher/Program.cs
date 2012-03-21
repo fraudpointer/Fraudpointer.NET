d72f96493987d738d9e6929b977ce8c565b55d1c
Panayotis Matsinopoulos <p.matsinopoulos@fraudpointer.com>
2012-03-21 17:54:56 +0200

    Fixes #8314 Console Application to give Credit Card Hashes

---
+++ CreditCardHasher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreditCardHasher
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Usave: cchash <cc> <cc> <cc> <cc> ...");    
+                Console.WriteLine("You can use this program to hash a list of credit cards using Fraudpointer hash algorithm.");
+                return 1;
+            }
+
+            var client = Fraudpointer.API.ClientFactory.Construct("", "");
+            for (var i = 0; i < args.Length; i++)
+            {
+                var credit_card = args[i];
+                var cc_hash = client.CreditCardHash(credit_card);
+                Console.WriteLine(credit_card + "\t" + cc_hash);
+            }
+            return 0;
+        }
+    }
+}
